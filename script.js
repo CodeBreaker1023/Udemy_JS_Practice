036eda9603e966e19dd301dd5bb18a71699d4598
ANS NEOphyte <jpbrown1023@gmail.com>
2019-02-09 17:12:06 -0500

    arrays

--- script.js
+++ script.js
@@ -32,20 +32,58 @@
 // function whatDoYouDo = function(job, firstName) {}
 
 //Function expression - are pieces of code that always produce a value
-var whatDoYouDo = function(job, firstName){
-    switch(job) {
-        case 'teacher':
-            return firstName + ' teaches kids how to code.';
-            //No break needed because return completes the function
-        case 'driver':
-            return firstName + ' drives an Uber in Lisbon.';
-        case 'designer':
-            return firstName + ' designs webpages.';
-        default:
-            return firstName + ' does something else.';
-    }
-}
+// var whatDoYouDo = function(job, firstName){
+//     switch(job) {
+//         case 'teacher':
+//             return firstName + ' teaches kids how to code.';
+//             //No break needed because return completes the function
+//         case 'driver':
+//             return firstName + ' drives an Uber in Lisbon.';
+//         case 'designer':
+//             return firstName + ' designs webpages.';
+//         default:
+//             return firstName + ' does something else.';
+//     }
+// }
 
-console.log(whatDoYouDo('teacher', 'John'));
-console.log(whatDoYouDo('retired', 'Mike'));
-console.log(whatDoYouDo('teacher', 'Mary'));
+// console.log(whatDoYouDo('teacher', 'John'));
+// console.log(whatDoYouDo('retired', 'Mike'));
+// console.log(whatDoYouDo('teacher', 'Mary'));
+
+/* ********* ARRAYS ************** */
+
+// Initialize new array
+var names = ['John', 'Mike', 'Mary'];
+var years = new Array (1990, 1948, 1969);
+
+console.log(names);
+console.log(names.length);
+
+// Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Jane';
+console.log(names);
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'teacher', false];
+
+// .push adds item to end of array
+john.push('blue');
+// .unshit adds item to the beginning of an array
+john.unshift('Mr.');
+console.log(john);
+
+// .pop removes the last item of an array
+john.pop();
+john.pop();
+// .shift removes the first item of an array
+john.shift();
+console.log(john);
+
+// indexOf identifies the position of item in an array; it will return -1 if not present
+console.log(john.indexOf(1990))
+
+// ? = then, : = else
+// If 'designer' is not present in the array then (?) 'John is NOT a designer' else (:) 'John IS a designer'
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
+console.log(isDesigner);    
