9191678568e72230bd0d33b67bbb6eb02f3a7838
ANS NEOphyte <jpbrown1023@gmail.com>
2019-02-09 19:30:46 -0500

    loops and iterations

--- script.js
+++ script.js
@@ -155,36 +155,70 @@
 // console.log(john.calculateAge(1990));
 // john.age = john.calculateAge();
 
-/* ******* BMI OBJECT CHALLENGE *********** */
-var john = {
-    fullName:'John Smith',
-    mass: 90,
-    height: 1.8,
-    calcBMI: function() {
-        this.bmi = this.mass / (this.height * this.height);
-        return this.bmi;
-    }
-}
+// /* ******* BMI OBJECT CHALLENGE *********** */
+// var john = {
+//     fullName:'John Smith',
+//     mass: 90,
+//     height: 1.8,
+//     calcBMI: function() {
+//         this.bmi = this.mass / (this.height * this.height);
+//         return this.bmi;
+//     }
+// }
 
-var mark = {
-    fullName:'Mark Morrison',
-    mass: 110,
-    height: 2.2,
-    calcBMI: function() {
-        this.bmi = this.mass / (this.height * this.height);
-        return this.bmi;
-    }
-}
+// var mark = {
+//     fullName:'Mark Morrison',
+//     mass: 110,
+//     height: 2.2,
+//     calcBMI: function() {
+//         this.bmi = this.mass / (this.height * this.height);
+//         return this.bmi;
+//     }
+// }
 
-john.calcBMI();
-mark.calcBMI();
-console.log(john, mark);
+// john.calcBMI();
+// mark.calcBMI();
+// console.log(john, mark);
 
-if (john.bmi > mark.bmi) {
-    console.log(john.fullName + ' has a higher BMI at ' + john.bmi + '.')
-} else if (mark.bmi > john.bmi) {
-    console.log(mark.fullName + ' has a higher BMI at ' + mark.bmi + '.')
-} else {
-    console.log('Mark and John have the same BMIs.')
-}
+// if (john.bmi > mark.bmi) {
+//     console.log(john.fullName + ' has a higher BMI at ' + john.bmi + '.')
+// } else if (mark.bmi > john.bmi) {
+//     console.log(mark.fullName + ' has a higher BMI at ' + mark.bmi + '.')
+// } else {
+//     console.log('Mark and John have the same BMIs.')
+// }
  
+// /* ******** Loops and Iterations ********** */
+
+// for (var i=0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+// for (var i = 0; i < john.length; i++) {
+//     console.log(john[i]);
+// }
+
+// // While loop
+// var i = 0;
+// while(i < john.length) {
+//     console.log(john[i]);
+//     i++;
+// }
+
+// Continue and Break statements
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+// for (var i = 0; i < john.length; i++) {
+//     if (typeof john[i] !== 'string') continue;
+//     console.log(john[i]);
+// }
+
+// for (var i = 0; i < john.length; i++) {
+//     if (typeof john[i] !== 'string') break;
+//     console.log(john[i]);
+// }
+
+// Looping backwards
+for (var i = john.length - 1; i >= 0; i--) {
+    console.log(john[i]);
+}
