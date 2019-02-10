1bf8b64aba388ade02833fc1296889dcedcae870
ANS NEOphyte <jpbrown1023@gmail.com>
2019-02-09 20:17:33 -0500

    "added mark"

--- script.js
+++ script.js
@@ -255,3 +255,31 @@ var john = {
 
 john.calculateTip();
 console.log(john);
+
+var mark = {
+    fullName: 'Mark Morrison',
+    bills: [77, 475, 110, 45],
+    calculateTip: function () {
+        this.tips = [];
+        this.totalBills = [];
+
+        for (var i = 0; i < this.bills.length; i++) {
+            // Determine percentage base on tipping rules
+            var percentage;
+            var bill = this.bills[i];
+
+            if (bill < 100) {
+                percentage =.2;
+            }
+            else if (bill >=100 && bill <= 300) {
+                percentage =.1;    
+            } else {
+                percentage=.25;  
+            }
+
+            // Add results to corresponding arrays
+            this.tips[i] = bill * percentage;
+            this.totalBills[i] = bill + bill * percentage;
+        }
+    }
+}
