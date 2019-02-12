0ea2034d3ebd34ff36776f3dc93ef6084fbde36f
ANS NEOphyte <jpbrown1023@gmail.com>
2019-02-12 06:50:18 -0500

    added toggle to player turn, dice now disappears between turns

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0,0]
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 // dice = Math.floor(Math.random() * 6) + 1;   
 
@@ -54,10 +54,25 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     if (dice !== 1){
         // Add score
         roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
 
     } else {
         // Next player
+        // Ternary operator vs if/else statement
+        // if activePlayer = 0 then activePlayer = 1, else activePlayer = 0
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+    
+        document.querySelector('.dice').style.display = 'none';
     }
 
 })
